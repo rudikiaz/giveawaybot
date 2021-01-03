5f4090d1e9a79d4f7ef7d6fd7ee721d292cb7760
Mario <rudikiaz@gmail.com>
2021-01-03 03:10:35 +0100

    Logeo del usuario en twitter en el navegador headless

--- twitterLogin.js
+++ twitterLogin.js
@@ -1,21 +1,22 @@
 const propertiesReader = require('properties-reader');
 const properties = propertiesReader('credentials.properties');
-console.log(properties)
-
 const { Chromeless } = require('chromeless')
 
-async function run() {
-  const chromeless = new Chromeless({launchChrome: false})
+
+
+async function run() { 
+  const chromeless = new Chromeless({launchChrome: true})
   
-  const screenshot = await chromeless
+  const twitterLogin = await chromeless
     .setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
-    .goto('https://www.twitter.com')
-    .type('chromeless', 'input[name="q"]')
-    .press(13)
-    .wait('#resultStats')
-
-  console.log("hola") 
-
+    .goto('https://twitter.com/login')
+    .wait(800)
+    .type(properties.get('username'), 'input[name="session[username_or_email]"]')
+    .wait(300)
+    .type(properties.get('password'), 'input[name="session[password]"]')
+    .wait(250)
+    .click('div[class="css-18t94o4 css-1dbjc4n r-urgr8i r-42olwf r-sdzlij r-1phboty r-rs99b7 r-1w2pmg r-vlx1xi r-zg41ew r-1jayybb r-17bavie r-1ny4l3l r-15bsvpr r-o7ynqc r-6416eg r-lrvibr"]')
+  
   await chromeless.end()
 }
 
